248266c57b8f0fe2cd0fe0320827f3a5eb057b1a
Thomas Stephens <spiralman@gmail.com>
2014-02-16 14:09:30 -0600

    Fix bug where entrants only in showcakes and/or tasting don't get judging forms

--- form_generator/form_generator.py
+++ form_generator/form_generator.py
@@ -378,8 +378,7 @@ if __name__ == "__main__":
 		# Put all of the contestant's judging sheets together
 		for entry in contestant.get('entries'):
 			signup = contestant.get('signup')
-			if (signup.get('class')):
-				if ((signup['class'] != 'Child') and (signup['class'] != 'Junior')):
-					generate_judging_form(canvas, contestant.get('signup'), entry)
-					canvas.showPage()
+                        if ((signup.get('class') != 'Child') and (signup.get('class') != 'Junior')):
+                                generate_judging_form(canvas, contestant.get('signup'), entry)
+                                canvas.showPage()
 	canvas.save()
